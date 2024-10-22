7172aed00cff04bc935eb48b3d128941938b15a8
ZsJuhaszZalan <zsiros-juhasz.zalan@diak.szbi-pg.hu>
2024-10-22 10:45:35 +0200

    uj

--- app.py
+++ app.py
@@ -21,7 +21,27 @@ def get_products():
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        user_data = {
+            "fullname": request.form.get("fullname"),
+            "email": request.form.get("email"),
+            "password": request.form.get("password"),
+            "phone": request.form.get("phone"),
+            "gender": request.form.get("gender"),
+            "color": request.form.get("color"),
+            "profile-pic": request.form.get("profile-pic"),
+            "Born-date": request.form.get("Born-date"),
+            "Actual-date": request.form.get("Actual-date"),
+            "interests": request.form.get("interests")
+        }
+        user.append(user_data)
+        return redirect(url_for('index'))
     return render_template('signup_form.html')
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', users=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
